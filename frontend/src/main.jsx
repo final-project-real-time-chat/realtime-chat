376735517311a2b88ce965a61ac4a1c7a97cf26c
Khambazarov <khambazarovrenat@gmail.com>
2025-02-24 10:55:27 +0100

    fix: localhost

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-io("http://localhost:3030");
+// io("http://localhost:3030");
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
