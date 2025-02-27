cfa173a057aaa3ee2f72353e02a2ca28c0ee3a9b
Khambazarov <khambazarovrenat@gmail.com>
2025-02-27 11:45:16 +0100

    change style: ForgotPw.jsx

--- frontend/src/components/ForgotPw.jsx
+++ frontend/src/components/ForgotPw.jsx
@@ -52,15 +52,29 @@ export const ForgotPw = () => {
         <label htmlFor="email" className="block text-gray-700 font-semibold">
           Email
         </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Enter your email"
-          className="text-black w-full p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          required
-          autoFocus
-        />
+        <div className="relative mb-4">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+            <svg
+              className="w-5 h-5 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 16"
+            >
+              <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+              <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+            </svg>
+          </div>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            className="bg-gray-50 focus:border-blue-500 block ps-10 text-black w-full p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+            autoFocus
+          />
+        </div>
         <button
           type="submit"
           className="cursor-pointer w-full bg-blue-600 text-white p-2 rounded-lg font-bold hover:bg-blue-700"
