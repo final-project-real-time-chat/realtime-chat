5071cf6b0c91ce5cefb294a6d52ab0d77679dc6b
Khambazarov <khambazarovrenat@gmail.com>
2025-03-04 16:02:12 +0100

    fix styles

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -37,9 +37,9 @@ export const AboutUs = () => {
           </svg>
         </button>
       </header>
-      <div className="p-8 flex flex-col items-center">
-        <div className="max-w-3xl">
-          <h1 className="text-3xl font-bold mb-2 text-center">About Us</h1>
+      <div className="py-8 max-w-3xl flex flex-col items-center">
+        <div className="border-2 max-w-fit">
+          <h1 className="text-3xl font-bold mb-2">About Us</h1>
           <p className="mb-4 text-balance">
             We, Olivia and Renat, are passionate developers who took on the
             challenge of creating a powerful and user-friendly real-time chat
@@ -48,96 +48,85 @@ export const AboutUs = () => {
             Our goal was to build a modern, fast, and secure communication
             platform.
           </p>
-          <h2 className="text-2xl font-semibold mb-2 mt-12 text-center">
-            App Development
-          </h2>
-          <p className="mb-4 text-balance">
-            Our real-time chat application is built on the MERN stack (MongoDB,
-            Express, React, Node.js). By utilizing cutting-edge technologies and
-            best practices, we developed a robust and scalable architecture.
-          </p>
-          <h2 className="text-2xl font-semibold mb-2 mt-12 text-center">
-            Technologies Used
-          </h2>
-          <ul className="list-disc list-inside mb-4">
-            <li className="mb-2 text-balance">
-              Frontend: React with Vite for an optimized development
-              environment, Zustand for state management, and Tailwind
-              CSS/DaisyUI for an appealing UI design.
-            </li>
-            <li className="mb-2 text-balance">
-              Backend: Express.js as the web framework, MongoDB as the NoSQL
-              database, bcrypt for password encryption, and JSON Web Token (JWT)
-              for secure authentication.
-            </li>
-            <li className="mb-2 text-balance">
-              Real-time Communication: Socket.io enables smooth real-time
-              messaging.
-            </li>
-            <li className="mb-2 text-balance">
-              Cloud Integration: Cloudinary is used for storing and managing
-              images.
-            </li>
-          </ul>
-          <h2 className="text-2xl font-semibold mb-2 mt-12 text-center">
-            App Features
-          </h2>
-          <ul className="list-disc list-inside mb-4">
-            <li className="mb-2 text-balance">
-              Our application allows users to register, log in, and communicate
-              in real-time. Key features include:
-            </li>
-            <li className="mb-2 text-balance">
-              User registration and login with secure authentication
-            </li>
-            <li className="mb-2 text-balance">
-              Real-time messaging powered by Socket.io
-            </li>
-            <li className="mb-2 text-balance">
-              Image uploads and media support
-            </li>
-            <li className="mb-2 text-balance">
-              Modern user interface, optimized for both desktop and mobile
-              devices
-            </li>
-          </ul>
-          <h2 className="text-2xl font-semibold mb-2 mt-12 text-center">
-            Project Links
-          </h2>
-          <ul className="list-disc list-inside mb-4">
-            <li className="mb-2  w-80">
-              <span className="inline-block w-40">Live Demo – Olivia:</span>
-              <a
-                href="https://hello-word-6z2bg.ondigitalocean.app/"
-                className="text-blue-400 underline"
-              >
-                Hello-Word App
-              </a>
-            </li>
-            <li className="mb-2 w-80">
-              <span className="inline-block w-40">Live Demo – Renat:</span>
-              <a
-                href="https://hello-word.khambazarov.dev/"
-                className="text-blue-400 underline"
-              >
-                Hello-Word App
-              </a>
-            </li>
-            <li className="mb-2 w-80">
-              <span className="inline-block w-40">Source Code:</span>
-              <a
-                href="https://github.com/final-project-real-time-chat/realtime-chat"
-                className="text-blue-400 underline"
-              >
-                GitHub Repository
-              </a>
-            </li>
-          </ul>
-          <h3 className="my-12 text-xl text-center text-balance">
-            We appreciate feedback and suggestions and warmly invite developers
-            to contribute to the further development of the app!
-          </h3>
         </div>
+        <h2 className="text-2xl font-semibold mb-2">App Development</h2>
+        <p className="mb-4 text-balance">
+          Our real-time chat application is built on the MERN stack (MongoDB,
+          Express, React, Node.js). By utilizing cutting-edge technologies and
+          best practices, we developed a robust and scalable architecture.
+        </p>
+        <h2 className="text-2xl font-semibold mb-2">Technologies Used</h2>
+        <ul className="list-disc list-inside mb-4">
+          <li className="mb-2 text-balance">
+            Frontend: React with Vite for an optimized development environment,
+            Zustand for state management, and Tailwind CSS/DaisyUI for an
+            appealing UI design.
+          </li>
+          <li className="mb-2 text-balance">
+            Backend: Express.js as the web framework, MongoDB as the NoSQL
+            database, bcrypt for password encryption, and JSON Web Token (JWT)
+            for secure authentication.
+          </li>
+          <li className="mb-2 text-balance">
+            Real-time Communication: Socket.io enables smooth real-time
+            messaging.
+          </li>
+          <li className="mb-2 text-balance">
+            Cloud Integration: Cloudinary is used for storing and managing
+            images.
+          </li>
+        </ul>
+        <h2 className="text-2xl font-semibold mb-2">App Features</h2>
+        <p className="mb-2 text-balance">
+          Our application allows users to register, log in, and communicate in
+          real-time. Key features include:
+        </p>
+        <ul className="list-disc list-inside mb-4">
+          <li className="mb-2 text-balance">
+            User registration and login with secure authentication
+          </li>
+          <li className="mb-2 text-balance">
+            Real-time messaging powered by Socket.io
+          </li>
+          <li className="mb-2 text-balance">Image uploads and media support</li>
+          <li className="mb-2 text-balance">
+            Modern user interface, optimized for both desktop and mobile devices
+          </li>
+        </ul>
+        <h2 className="text-2xl font-semibold mb-2">Project Links</h2>
+        <ul className="list-disc list-inside mb-4">
+          <li className="mb-2  w-76">
+            <span className="inline-block w-36">Live Demo – Olivia:</span>
+            <a
+              href="https://hello-word-6z2bg.ondigitalocean.app/"
+              className="text-blue-400 underline"
+            >
+              Hello-Word App
+            </a>
+          </li>
+          <li className="mb-2 w-76">
+            <span className="inline-block w-36">Live Demo – Renat:</span>
+            <a
+              href="https://hello-word.khambazarov.dev/"
+              className="text-blue-400 underline"
+            >
+              Hello-Word App
+            </a>
+          </li>
+          <li className="mb-2 w-76">
+            <span className="inline-block w-36">Source Code:</span>
+            <a
+              href="https://github.com/final-project-real-time-chat/realtime-chat"
+              className="text-blue-400 underline"
+            >
+              GitHub Repository
+            </a>
+          </li>
+        </ul>
+        <h3 className="text-xl text-center text-balance">
+          We appreciate feedback and suggestions and warmly invite developers to
+          contribute to the further development of the app!
+        </h3>
       </div>
     </div>
   );
